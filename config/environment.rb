ffbccae74c28b118e8d9a14a085efbb705723e81
Nils-Lopez <nils.lpzz@gmail.com>
2020-05-11 13:08:34 -0700

    stackover

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+ActionMailer::Base.smtp_settings =   {
+      address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_LOGIN"],
   password: ENV["GMAIL_PWD"]
-}
+  }
