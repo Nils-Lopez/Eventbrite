25319a3779507978210d6308a0077388de3e9cc4
Nils-Lopez <nils.lpzz@gmail.com>
2020-05-11 04:00:37 -0700

    with mailer

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
-	validates: start_date, presence: true
-	validates: title, presence: true, length: { minimum: 5, maximum: 140 }
-	validates: description, presence: true, length: { minimum: 20, maximum: 1000 }
-	validates: price, inclusion: {in: 1..1000}
-	validates: location, presence: true
-	validate: duration_5
-	validate: start_date_after_now
+	validates :start_date, presence: true
+	validates :title, presence: true, length: { minimum: 5, maximum: 140 }
+	validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
+	validates :price, inclusion: {in: 1..1000}
+	validates :location, presence: true
+	validate :duration_5
+	validate :start_date_after_now
 
 	private
 
